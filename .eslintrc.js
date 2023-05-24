0af5b522b884dd1a06a5ad2de9915194ee3237b6
senasoon <senasoon0@gmail.com>
2023-05-24 14:51:57 +0900

    init: project init

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  ignorePatterns: ['.eslintrc.js'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-var': 'error', // var 금지
+    'no-multiple-empty-lines': 'error', // 여러 줄 공백 금지
+    'no-console': ['error', { allow: ['warn', 'error', 'info'] }], // console.log() 금지
+    eqeqeq: 'error', // 일치 연산자 사용 필수
+    'dot-notation': 'error', // 가능하다면 dot notation 사용
+    'no-unused-vars': 'error', // 사용하지 않는 변수 금지,
+    '@typescript-eslint/no-unsafe-assignment': 'warn',
+    '@typescript-eslint/no-unsafe-member-access': 'warn',
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  arrowParens: 'always',
+  htmlWhitespaceSensitivity: 'css',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  printWidth: 100,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  parser: 'typescript',
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+};
