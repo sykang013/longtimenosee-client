36220512fc6c03063b0895abd939a02c59fba480
senasoon <senasoon0@gmail.com>
2023-06-29 12:21:10 +0900

    feat: 자기소개 입력란 추가 #22

--- src/screens/auth/CreateProfileScreen.tsx
+++ src/screens/auth/CreateProfileScreen.tsx
@@ -3,20 +3,33 @@ import styled from 'styled-components/native';
 import { light } from '@/assets/themes';
 import { InputProfile } from '@/components/inputs';
 import { ButtonMain } from '@/components/buttons';
+import { View } from 'react-native';
 
-const MakeProfileScreen = () => {
+const CreateProfileScreen = () => {
   const [isNicknameActive, setIsNicknameActive] = useState(false);
+  const [isDescriptionActive, setIsDescriptionActive] = useState(false);
+
   return (
     <StContainer>
       <StProfile></StProfile>
-      <InputProfile
-        placeholder="닉네임은 최소 두글자 이상이어야 합니다."
-        isActive={isNicknameActive}
-        onFocus={() => setIsNicknameActive(true)}
-        onBlur={() => setIsNicknameActive(false)}
-        label="닉네임"
-        maxLength={15}
-      />
+      <View>
+        <InputProfile
+          placeholder="닉네임은 최소 두글자 이상이어야 합니다."
+          isActive={isNicknameActive}
+          onFocus={() => setIsNicknameActive(true)}
+          onBlur={() => setIsNicknameActive(false)}
+          label="닉네임"
+          maxLength={15}
+        />
+        <InputProfile
+          placeholder="자기소개를 입력해보세요."
+          isActive={isDescriptionActive}
+          onFocus={() => setIsDescriptionActive(true)}
+          onBlur={() => setIsDescriptionActive(false)}
+          label="자기소개"
+          maxLength={100}
+        />
+      </View>
       <ButtonMain buttonState="ActivePrimary" width={312}>
         확인
       </ButtonMain>
@@ -24,7 +37,7 @@ const MakeProfileScreen = () => {
   );
 };
 
-export default MakeProfileScreen;
+export default CreateProfileScreen;
 
 const StContainer = styled.View`
   flex: 1;
