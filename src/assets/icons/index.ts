3c512644682c0c445dbcf3be4090ce1bd5ff0d69
ChulJin Lim <cjfwls9806@gmail.com>
2023-07-08 21:09:17 +0900

    feat: svg icon 추가 #28

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,29 @@
+import CheckFalseGray from './CheckFalseGray';
+import CheckFalsePrimary from './CheckFalsePrimary';
+import CheckTrue from './CheckTrue';
+import IconCalendar from './IconCalendar';
+import IconChat from './IconChat';
+import IconCreate from './IconCreate';
+import IconGroup from './IconGroup';
+import IconHamburger from './IconHamburger';
+import IconHome from './IconHome';
+import IconLocation from './IconLocation';
+import IconNotification from './IconNotification';
+import IconProfile from './IconProfile';
+import IconSearch from './IconSearch';
+
+export {
+  CheckFalseGray,
+  CheckFalsePrimary,
+  CheckTrue,
+  IconCalendar,
+  IconChat,
+  IconCreate,
+  IconGroup,
+  IconHamburger,
+  IconHome,
+  IconLocation,
+  IconNotification,
+  IconProfile,
+  IconSearch,
+};
