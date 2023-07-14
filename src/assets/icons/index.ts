90cd80934261a5ffee66d96f3cce6427f8206e28
senasoon <senasoon0@gmail.com>
2023-07-15 00:16:56 +0900

    refactor: check icon 파일 이름 변경, color props로 수정 #32

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -4,5 +4,16 @@ import IconLocationLarge from './IconLocationLarge';
 import IconLocationMedium from './IconLocationMedium';
 import IconOnline from './IconOnline';
 import IconSearch from './IconSearch';
+import IconCheckFalse from './IconCheckFalse';
+import IconCheckTrue from './IconCheckTrue';
 
-export { IconLocationLarge, IconLocationMedium, IconClose, IconSearch, IconOnline, IconClock };
+export {
+  IconLocationLarge,
+  IconLocationMedium,
+  IconClose,
+  IconSearch,
+  IconOnline,
+  IconClock,
+  IconCheckFalse,
+  IconCheckTrue,
+};
